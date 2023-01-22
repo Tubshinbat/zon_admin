284082c9eeccc9a4d6a00511b3548158519b9a73
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-01-23 05:29:37 +0800

    change urls

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://admin.zaya-ananda.com/api/",
-  baseURL: "http://localhost:8060/api/v1/",
+  baseURL: "https://adminbeta.metaldoor.mn/api/",
+  // baseURL: "http://localhost:8060/api/v1/",
 });
 
 instance.defaults.withCredentials = true;
--- src/base.js
+++ src/base.js
@@ -1,8 +1,12 @@
 const base = {
-  siteUrl: "http://localhost:3000/",
-  baseUrl: "http://localhost:3001/",
-  cdnUrl: "http://localhost:8060/uploads/",
-  apiUrl: "http://localhost:8060/api/v1/",
+  // siteUrl: "http://localhost:3000/",
+  // baseUrl: "http://localhost:3001/",
+  // cdnUrl: "http://localhost:8060/uploads/",
+  // apiUrl: "http://localhost:8060/api/v1/",
+  siteUrl: "https://beta.metaldoor.mn",
+  baseUrl: "https://adminbeta.metaldoor.mn",
+  cdnUrl: "https://beta.metaldoor.mn/uploads/",
+  apiUrl: "https://adminbeta.metaldoor.mn/api/",
 };
 
 export default base;
