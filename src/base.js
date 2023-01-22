d4c8868f7cdd036edff55a21cb925d1a7d56aedf
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-01-23 05:41:51 +0800

    change baseurl

--- src/base.js
+++ src/base.js
@@ -4,7 +4,7 @@ const base = {
   // cdnUrl: "http://localhost:8060/uploads/",
   // apiUrl: "http://localhost:8060/api/v1/",
   siteUrl: "https://beta.metaldoor.mn",
-  baseUrl: "https://adminbeta.metaldoor.mn",
+  baseUrl: "https://adminbeta.metaldoor.mn/",
   cdnUrl: "https://beta.metaldoor.mn/uploads/",
   apiUrl: "https://adminbeta.metaldoor.mn/api/",
 };
